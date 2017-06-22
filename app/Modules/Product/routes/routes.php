df96d142fc9d7c5ff041a902d5bbb20370acebaa
Muhammad Anwar <m_anwar@awok.ae>
2017-06-22 13:05:18 +0400

    Adding product delete route

--- app/Modules/Product/routes/routes.php
+++ app/Modules/Product/routes/routes.php
@@ -12,7 +12,7 @@ $app->group(['middleware' => ['auth:api', 'role:admin|seller']], function () use
     // Product Routes
     $app->post('/', 'ProductController@create');
     $app->put('/{id}', 'ProductController@update');
-    $app->put('/{id}', 'ProductController@delete');
+    $app->delete('/{id}', 'ProductController@delete');
     $app->get('/{id}', 'ProductController@get');
     $app->get('/{id}/attributes', 'ProductController@getProductAttributes');
     $app->get('/', 'ProductController@fetch');
